3702431d66aac9038f40b5b48eaff8e7a4a72fae
Ricardogs2002 <ricardogs2002@gmail.com>
2023-02-22 17:21:42 -0600

    0001_Ricardo_Ismael_Gomez_Sanchez
    Capítulos 2 a 6, strings

---
+++ 2023_02_22_String_V1.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 22 09:44:20 2023
+
+@author: Ricardo Ismael Gomez Sanchez
+"""
+#1
+cap_2 = "Hola mundo"
+#2
+x = 10
+y = 15
+#3
+resultado = x + y
+#4
+print(resultado,cap_2)
+#5
+csimple = "Dato 'relevante' "
+cdoble = 'Noten el "sarcasmo"'
+print(csimple,cdoble)
+#6
+cap_3 = '"print()" se utiliza para imprimir valores en la consola.'
+print(cap_3)
+#7
+frase = "Hola"+" "+"k hace"
+print(frase)
+#8
+combo = csimple + cdoble
+print(combo)
+#9
+algo = "Buen"
+print(algo+" día")
+#10
+n1 = "Ricardo"
+n2 = "Ismael"
+n3 = "Gómez"
+n4 = "Sánchez ♠"
+ntotal = n1+" "+n2+" "+n3+" "+n4
+print(ntotal)
+#11
+print('4'+"7")
+#12
+title = "ricardo ismael gómez sánchez".title()
+#13-14
+oracion = "Esta Es Una Frase Para Ser Formateada."
+upper = oracion.upper()
+lower = oracion.lower()
+print(title+"\n"+upper+"\n"+lower)
+#15
+print("\t-Python.\n\t-JavaScript.\n\t-Java.\n\t-PHP.\n\t-TypeScript.\n\t-SQL.\n\t-COBOL.\n\t-SQL.\n\t-C#.\n\t-R.")

6bea706b288f91c78360a480e2793885a6f3e779
Ricardogs2002 <ricardogs2002@gmail.com>
2023-02-23 09:47:06 -0600

    0002_Riccardo_Ismael_Gomez_Sanchez
    Capítulos  5 a 9, operadores

---
+++ 2023_02_23_Operadores_V1.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Feb 22 21:13:19 2023
+
+@author: Ricardo Ismael Gomez Sanchez
+"""
+
+#16
+suma = 20+23+14+30
+
+#17
+resta = 20-23-4-80
+
+#18
+multi = 10*8.7*10
+
+#19
+divi = 5000/(230+270)
+print("suma:",suma,"\nresta:",resta,"\nmultiplicación:",multi,"\ndivisión:",divi)
+
+#20
+operacion = 10 / 5 + 15 - 17
+print("operación",operacion)
+
+#21
+valor = 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2
+valore= 2**10
+print("Sin exponente:",valor,": con exponente:",valore)
+
+#22
+numero = round(17.567383292929200234, 5)
+print(numero)
